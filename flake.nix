59c245ab4302ea9729349588a4831c00bd0d849b
Soliprem <franci.solidoro@gmail.com>
2025-08-12 14:54:20 +0200

    much better flake

--- flake.nix
+++ flake.nix
@@ -3,33 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustc
-          cargo
-          nushell
-        ];
-        shellHook = ''
-          exec nu
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    naersk,
+    fenix,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    rustToolchain = fenix.packages.x86_64-linux.default.toolchain;
+  in {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustc
+        cargo
+        nushell
+      ];
+      shellHook = ''
+        exec nu
+      '';
+    };
 
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+    packages.${system}.default =
+      (naersk.lib.x86_64-linux.override {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      }).buildPackage {
         pname = "rpg_quiz";
         version = "0.1.0";
-        
         src = ./.;
-
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
       };
-    };
+  };
 }
